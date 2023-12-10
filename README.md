56b050359bd84514510ee5e6ba3558424ee4f35d
tony-cannibal <castrovital.luis@gmail.com>
2023-12-09 18:26:54 -0600

    Update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # qtile Config
+
+requires psutil
+
+```
+pacman -S python-psutil
+pip install pulsectl-asyncio --break-system-packages
+```
--- config.py
+++ config.py
@@ -27,17 +27,17 @@
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
+# from libqtile.utils import guess_terminal
 import subprocess
 import os
 from colors import solarized_dark as colors
 
 mod = "mod4"
-terminal = "alacritty" 
+terminal = "alacritty"
 
 key_scripts = [
-        '~/.config/rofi/scripts/launcher_t2'
-        ]
+    '~/.config/rofi/scripts/launcher_t2'
+]
 
 keys = [
     # A list of available commands that can be bound to keys can be found
@@ -47,17 +47,22 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "l", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "h", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -71,18 +76,22 @@ keys = [
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod, "shift"], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod, "shift"], "Return", lazy.spawn(
+        terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
 
-    Key([mod], "p", lazy.spawn(os.path.expanduser('~/.config/rofi/scripts/launcher_t2')), desc="rofi"),
+    Key([mod], "p", lazy.spawn(os.path.expanduser(
+        '~/.config/rofi/scripts/launcher_t2')), desc="rofi"),
     Key([mod], "period", lazy.screen.next_group(), desc="Next Workspace"),
     Key([mod], "comma", lazy.screen.prev_group(), desc="Previous Workspace"),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle Focused Floating"),
-    Key([mod], "x", lazy.spawn(os.path.expanduser("~/.config/rofi/scripts/powermenu_t2")), desc="Power Menu"),
+    Key([mod], "t", lazy.window.toggle_floating(),
+        desc="Toggle Focused Floating"),
+    Key([mod], "x", lazy.spawn(os.path.expanduser(
+        "~/.config/rofi/scripts/powermenu_t2")), desc="Power Menu"),
     Key([mod], "kp_add", lazy.spawn("pamixer -i 5"), desc="Power Menu"),
     Key([mod], "kp_subtract", lazy.spawn("pamixer -d 5"), desc="Power Menu"),
 ]
@@ -104,6 +113,7 @@ groups = [__groups[i] for i in __groups]
 def get_group_key(name):
     return [k for k, g in __groups.items() if g.name == name][0]
 
+
 for i in groups:
     keys.extend(
         [
@@ -111,13 +121,14 @@ for i in groups:
             Key([mod], str(get_group_key(i.name)),
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
-            ),
+                ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                desc="Switch to & move focused window to group {}".format(
+                    i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
@@ -128,8 +139,8 @@ for i in groups:
 
 
 window_options = {
-    'border_width':2,
-    'margin':6,
+    'border_width': 2,
+    'margin': 6,
 }
 
 window_options['border_focus'] = colors['13']
@@ -153,7 +164,7 @@ layouts = [
 
 widget_defaults = dict(
     font="terminus",
-    fontsize=12,
+    fontsize=14,
     padding=4,
     foreground=colors['13'],
     background=colors['1'],
@@ -170,8 +181,8 @@ screens = [
                     this_current_screen_border=colors['13'],
                     # rounded=False,
                     highlight_method='text',
-                    active=colors['7'], 
-                    ##inactive=colors['1'],
+                    active=colors['7'],
+                    # inactive=colors['1'],
                 ),
                 widget.Sep(padding=6),
                 widget.WindowName(),
@@ -179,18 +190,21 @@ screens = [
                 # File System Script
                 widget.GenPollText(
                     update_interval=3,
-                    func=lambda: subprocess.check_output(os.path.expanduser('~/.config/qtile/bar_scripts/hdd-space.sh')).decode("utf-8")
+                    func=lambda: subprocess.check_output(os.path.expanduser(
+                        '~/.config/qtile/bar_scripts/hdd-space.sh')).decode("utf-8")
                 ),
                 widget.Sep(padding=6),
                 widget.PulseVolume(
                     fmt="VOL {}",
                     update_interval=0.1,
+                    limit_max_volume=True
                 ),
                 widget.Sep(padding=6),
                 # Ram Script
                 widget.GenPollText(
                     update_interval=3,
-                    func=lambda: subprocess.check_output(os.path.expanduser('~/.config/qtile/bar_scripts/mem.sh')).decode("utf-8")
+                    func=lambda: subprocess.check_output(os.path.expanduser(
+                        '~/.config/qtile/bar_scripts/mem.sh')).decode("utf-8")
                 ),
                 widget.Sep(padding=6),
                 widget.KeyboardLayout(),
@@ -201,15 +215,18 @@ screens = [
             ],
             24,
             border_width=[2, 2, 2, 2],  # Draw top and bottom borders
-            border_color=[colors['13'], colors['13'], colors['13'], colors['13']]  # Borders are magenta
+            border_color=[colors['13'], colors['13'],
+                          colors['13'], colors['13']]  # Borders are magenta
         ),
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -254,11 +271,12 @@ wmname = "LG3D"
 
 startup = [
     'xset s off -dpms',
+    'xset r rate 350 60',
     'setxkbmap -option caps:swapescape',
     'numlockx &',
     'udiskie &',
     'lxpolkit &',
-    'feh --bg-fill ~/Pictures/wallpapers/Solarized/sentre.jpg',
+    'feh --bg-fill ~/Pictures/Wallpapers/Solarized/sentre.jpg &',
     'picom --config ~/.config/qtile/picom/picom.conf &',
     'mpd &'
 ]
